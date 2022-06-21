1d9182cdca876bd32143c0da4900ab3d8b23ab34
cscthms <alascahms@gmail.com>
2022-06-21 15:37:47 -0300

    Cutscenes e Bugs Resolvidos
    Arrumei os bugs da scene transition e melhorei a transição entre as cutscenes

--- athousandnightsunity/Assets/Cutscenes/LoadOnActivation.cs
+++ athousandnightsunity/Assets/Cutscenes/LoadOnActivation.cs
@@ -3,10 +3,14 @@ using UnityEngine.SceneManagement;
  
 public class LoadOnActivation : MonoBehaviour
 {
-    
+    public string sceneToLoad; 
+    public Vector2 playerPosition; 
+    public VectorValue playerStorage; 
     void OnEnable()
     {
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
         SceneManager.LoadScene("Water", LoadSceneMode.Single);
+         playerStorage.initialValue = playerPosition;
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
